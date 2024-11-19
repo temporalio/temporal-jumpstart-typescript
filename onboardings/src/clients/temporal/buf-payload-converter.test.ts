9350c64a8ca20232df29de2707bd9b714c08d6b0
Mike Nichols <mike.nichols@temporal.io>
2024-11-18 23:46:53 -0500

    tests for payload converter

---
+++ onboardings/src/clients/temporal/buf-payload-converter.test.ts
@@ -0,0 +1,92 @@
+import {FileDescriptorSetSchema} from '@bufbuild/protobuf/wkt'
+import { readFileSync } from 'node:fs';
+import {create, fromJsonString, isMessage, toJsonString} from '@bufbuild/protobuf'
+import {PingRequestSchema} from '@generated/onboardings/domain/v0/workflows_pb'
+import { PingRequest} from '@generated/onboardings/domain/v0/workflows_pb'
+import {
+  BufBinaryPayloadConverter,
+  BufJsonPayloadConverter,
+  DefaultPayloadConverterWithBufs
+} from './buf-payload-converter'
+import {Payload} from '@temporalio/common/lib'
+const { assert } = require('chai')
+
+const { createFileRegistry, fromBinary } = require('@bufbuild/protobuf')
+const { FileDescriptSetSchema } = require('@bufbuild/protobuf/wkt')
+// const { addition } = require('./calculator')
+
+describe("Registry Tests", () => {
+  it("should work", () => {
+    const fileDescriptorSet = fromBinary(
+      FileDescriptorSetSchema,
+      readFileSync("./descriptors.binpb"),
+    );
+    const registry = createFileRegistry(fileDescriptorSet);
+    for (const file of registry.files) {
+      //console.log('file', file.name);
+    }
+    for (const type of registry) {
+    //  console.log('type', type)
+      // type.kind; // "message" | "enum" | "extension" | "service"
+    }
+    let ping = create(PingRequestSchema, {
+      name: "monkey"
+    })
+
+    let raw = JSON.stringify(ping)
+    console.log('bare json', raw)
+    console.log('isMessage', isMessage(ping))
+    let desc = registry.getMessage(ping.$typeName)
+    let json = toJsonString(desc, ping)
+    console.log('json', json)
+
+    let ping2 = fromJsonString(registry.getMessage('temporal.onboardings.workflows.v0.PingRequest'), json )
+    console.log('ping2', ping2)
+    // const result = addition(2, 3);
+    // assert.equal(5, 5);
+  });
+  it('should support binary payloads', () => {
+    const fileDescriptorSet = fromBinary(
+      FileDescriptorSetSchema,
+      readFileSync("./descriptors.binpb"),
+    );
+    const registry = createFileRegistry(fileDescriptorSet);
+    let sut = new BufBinaryPayloadConverter(registry)
+    let ping = create(PingRequestSchema, {
+      name: "monkey"
+    })
+
+    let payload = sut.toPayload(ping)
+    assert.isDefined(payload)
+    // console.log('payload', payload)
+    if(!payload) {
+      assert.fail('payload must be defined')
+    }
+
+    let message: PingRequest = sut.fromPayload(payload as Payload)
+    assert.equal(message.name, ping.name)
+    console.log('message', message)
+  })
+  it('should support json payloads', () => {
+    const fileDescriptorSet = fromBinary(
+      FileDescriptorSetSchema,
+      readFileSync("./descriptors.binpb"),
+    );
+    const registry = createFileRegistry(fileDescriptorSet);
+    let sut = new BufJsonPayloadConverter(registry)
+    let ping = create(PingRequestSchema, {
+      name: "monkey"
+    })
+
+    let payload = sut.toPayload(ping)
+    assert.isDefined(payload)
+    // console.log('payload', payload)
+    if(!payload) {
+      assert.fail('payload must be defined')
+    }
+
+    let message: PingRequest = sut.fromPayload(payload as Payload)
+    assert.equal(message.name, ping.name)
+    console.log('message', message)
+  })
+});
--- onboardings/src/clients/temporal/buf-payload-converter.ts
+++ onboardings/src/clients/temporal/buf-payload-converter.ts
@@ -1,58 +1,231 @@
-// import {
-//   EncodingType,
-//   METADATA_ENCODING_KEY,
-//   Payload,
-//   PayloadConverterError,
-//   PayloadConverterWithEncoding,
-// } from '@temporalio/common'
-// import { decode, encode } from '@temporalio/common/lib/encoding.js'
-// import { toJson, toBinary, toJsonString } from '@bufbuild/protobuf'
-//
-// /**
-//  */
-// export class BufPayloadConverter implements PayloadConverterWithEncoding {
-//   // Use 'json/plain' so that Payloads are displayed in the UI
-//   public encodingType = 'json/plain' as EncodingType
-//
-//   public toPayload(value: unknown): Payload | undefined {
-//     if (value === undefined) return undefined
-//     let out
-//     try {
-//       out = toJsonString(value)
-//     }
-//     catch (e) {
-//       throw new UnsupportedEjsonTypeError(
-//         `Can't run EJSON.stringify on this value: ${value}. Either convert it (or its properties) to EJSON-serializable values (see https://docs.meteor.com/api/ejson.html ), or create a custom data converter. EJSON.stringify error message: ${
-//           errorMessage(
-//             e,
-//           )
-//         }`,
-//         e as Error,
-//       )
-//     }
-//
-//     return {
-//       metadata: {
-//         [METADATA_ENCODING_KEY]: encode('json/plain'),
-//         // Include an additional metadata field to indicate that this is an EJSON payload
-//         format: encode('extended'),
-//       },
-//       data: encode(ejson),
-//     }
-//   }
-//
-//   public fromPayload<T>(content: Payload): T {
-//     return content.data ? EJSON.parse(decode(content.data)) : content.data
-//   }
-// }
-//
-// export class UnsupportedEjsonTypeError extends PayloadConverterError {
-//   public readonly name: string = 'UnsupportedJsonTypeError'
-//
-//   constructor(
-//     message: string | undefined,
-//     public readonly cause?: Error,
-//   ) {
-//     super(message ?? undefined)
-//   }
-// }
+import { decode, encode } from '@temporalio/common/lib/encoding'
+import { PayloadConverterError, ValueError } from '@temporalio/common/lib/errors';
+import { Payload } from '@temporalio/common/lib';
+// import { errorMessage, hasOwnProperties, hasOwnProperty, isRecord } from '../type-helpers';
+
+import {
+  BinaryPayloadConverter,
+  CompositePayloadConverter,
+  JsonPayloadConverter,
+  PayloadConverterWithEncoding,
+  UndefinedPayloadConverter,
+} from '@temporalio/common/lib/converter/payload-converter';
+
+// import { encodingTypes, METADATA_ENCODING_KEY, METADATA_MESSAGE_TYPE_KEY } from './types';
+import {
+  DescEnum,
+  DescExtension,
+  DescField,
+  DescMessage,
+  DescService,
+  fromBinary, fromJsonString,
+  isMessage, Message,
+  Registry, toBinary, toJson, toJsonString
+} from '@bufbuild/protobuf'
+
+const GLOBAL_BUFFER = globalThis.constructor.constructor('return globalThis.Buffer')();
+const encodingTypes = {
+  METADATA_ENCODING_BUF_JSON : 'json/buf',
+  METADATA_ENCODING_BUF: 'binary/buf'
+}
+const METADATA_ENCODING_KEY = 'encoding'
+const METADATA_MESSAGE_TYPE_KEY = '$typeName'
+
+abstract class BufPayloadConverter implements PayloadConverterWithEncoding {
+  protected readonly registry: Registry;
+  public abstract encodingType: string;
+
+  public abstract toPayload<T>(value: T): Payload | undefined;
+  public abstract fromPayload<T>(payload: Payload): T;
+
+  // Don't use type Root here because root.d.ts doesn't export Root, so users would have to type assert
+  constructor(registry: Registry) {
+      this.registry = registry;
+  }
+
+  protected validatePayload(content: Payload): { messageType: DescMessage; data: Uint8Array } {
+    if (content.data === undefined || content.data === null) {
+      throw new ValueError('Got payload with no data');
+    }
+    if (!content.metadata || !(METADATA_MESSAGE_TYPE_KEY in content.metadata)) {
+      throw new ValueError(`Got protobuf payload without metadata.${METADATA_MESSAGE_TYPE_KEY}`);
+    }
+    if (!this.registry) {
+      throw new PayloadConverterError('Unable to deserialize protobuf message without `registry` being provided');
+    }
+
+    const messageTypeName = decode(content.metadata[METADATA_MESSAGE_TYPE_KEY]);
+    let messageType = this.registry.getMessage(messageTypeName);
+    if(!messageType) {
+      throw new PayloadConverterError(`Got a \`${messageTypeName}\` protobuf message but cannot find corresponding message class in \`registry\``)
+    }
+    return { messageType, data: content.data };
+  }
+
+  protected constructPayload({ messageTypeName, message }: { messageTypeName: string; message: Uint8Array }): Payload {
+    return {
+      metadata: {
+        [METADATA_ENCODING_KEY]: encode(this.encodingType),
+        [METADATA_MESSAGE_TYPE_KEY]: encode(messageTypeName),
+      },
+      data: message,
+    };
+  }
+}
+
+/**
+ * Converts between protobufjs Message instances and serialized Protobuf Payload
+ */
+export class BufBinaryPayloadConverter extends BufPayloadConverter {
+  public encodingType = encodingTypes.METADATA_ENCODING_BUF;
+
+  /**
+   * @param registry
+   */
+  constructor(registry: Registry) {
+    super(registry);
+  }
+
+  public toPayload(value: unknown): Payload | undefined {
+    let out = tryToMessage(this.registry, value)
+    if(!out) {
+      return undefined
+    }
+    let bytes = toBinary(out.schema, out.message)
+    return this.constructPayload({
+      messageTypeName: out.schema.typeName,
+      message: bytes,
+    });
+  }
+
+  public fromPayload<T>(content: Payload): T {
+    const { messageType, data } = this.validatePayload(content);
+    // Wrap with Uint8Array from this context to ensure `instanceof` works
+    const localData = data ? new Uint8Array(data.buffer, data.byteOffset, data.length) : data;
+    return fromBinary(messageType, localData) as unknown as T;
+    // return messageType.decode(localData) as unknown as T;
+  }
+}
+
+/**
+ * Converts between protobufjs Message instances and serialized JSON Payload
+ */
+export class BufJsonPayloadConverter extends BufPayloadConverter {
+  public encodingType = encodingTypes.METADATA_ENCODING_BUF_JSON;
+
+  /**
+   * @param registry
+   */
+  constructor(registry: Registry) {
+    super(registry);
+  }
+
+  public toPayload(value: unknown): Payload | undefined {
+    let out = tryToMessage(this.registry, value)
+    if (!out) {
+      return undefined;
+    }
+
+    const hasBufferChanged = setBufferInGlobal()
+    try {
+      const jsonValue = toJsonString(out.schema, out.message)
+
+      return this.constructPayload({
+        messageTypeName: out.schema.typeName,
+        message: encode(jsonValue),
+      });
+    } finally {
+      resetBufferInGlobal(hasBufferChanged);
+    }
+  }
+
+  public fromPayload<T>(content: Payload): T {
+    const hasBufferChanged = setBufferInGlobal();
+    try {
+      const { messageType, data } = this.validatePayload(content);
+      const res = fromJsonString(messageType, decode(data)) as unknown as T
+      if (Buffer.isBuffer(res)) {
+        return new Uint8Array(res) as any;
+      }
+      replaceBuffers(res);
+      return res;
+    } finally {
+      resetBufferInGlobal(hasBufferChanged);
+    }
+  }
+}
+
+function replaceBuffers<X>(obj: X) {
+  const replaceBuffersImpl = <Y>(value: any, key: string | number, target: Y) => {
+    if (Buffer.isBuffer(value)) {
+      // Need to copy. `Buffer` manages a pool slab, internally reused when Buffer objects are GC.
+      type T = keyof typeof target;
+      target[key as T] = new Uint8Array(value) as any;
+    } else {
+      replaceBuffers(value);
+    }
+  };
+
+  if (obj != null && typeof obj === 'object') {
+    // Performance optimization for large arrays
+    if (Array.isArray(obj)) {
+      obj.forEach(replaceBuffersImpl);
+    } else {
+      for (const [key, value] of Object.entries(obj)) {
+        replaceBuffersImpl(value, key, obj);
+      }
+    }
+  }
+}
+
+function setBufferInGlobal(): boolean {
+  if (typeof globalThis.Buffer === 'undefined') {
+    globalThis.Buffer = GLOBAL_BUFFER;
+    return true;
+  }
+  return false;
+}
+
+function resetBufferInGlobal(hasChanged: boolean): void {
+  if (hasChanged) {
+    delete (globalThis as any).Buffer;
+  }
+}
+
+function tryToMessage(registry: Registry, value: unknown): { message: Message, schema: DescMessage } | undefined {
+  if(!isMessage(value)) {
+    return undefined
+  }
+  let schema = registry.getMessage(value.$typeName)
+  if(!schema) {
+    return undefined
+  }
+  return { message: value, schema }
+}
+
+
+export interface DefaultPayloadConverterWithBufOptions {
+  /**
+   * The `registry` provided to {@link BufJsonPayloadConverter} and {@link BufBinaryPayloadConverter}
+   */
+  registry: Registry;
+}
+
+export class DefaultPayloadConverterWithBufs extends CompositePayloadConverter {
+  // Match the order used in other SDKs.
+  //
+  // Go SDK:
+  // https://github.com/temporalio/sdk-go/blob/5e5645f0c550dcf717c095ae32c76a7087d2e985/converter/default_data_converter.go#L28
+  constructor({ registry }: DefaultPayloadConverterWithBufOptions) {
+    if(!registry) {
+      throw new Error('registry is required')
+    }
+    super(
+      new UndefinedPayloadConverter(),
+      new BinaryPayloadConverter(),
+      new BufJsonPayloadConverter(registry),
+      new BufBinaryPayloadConverter(registry),
+      new JsonPayloadConverter()
+    );
+  }
+}
