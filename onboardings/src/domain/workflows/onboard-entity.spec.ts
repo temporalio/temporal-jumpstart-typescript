d7313733b8830ad5490bb3d9b2e889bf57ad59ef
Mike Nichols <mike.nichols@temporal.io>
2024-12-06 01:17:53 -0500

    added some tests for signal and query

--- onboardings/src/domain/workflows/onboard-entity.spec.ts
+++ onboardings/src/domain/workflows/onboard-entity.spec.ts
@@ -1,25 +1,69 @@
 import { TestWorkflowEnvironment } from '@temporalio/testing';
 import { before, describe, it } from 'mocha';
 import { Worker } from '@temporalio/worker';
-import {ERR_INVALID_ARGS, onboardEntity} from './onboard-entity'
+import {ERR_INVALID_ARGS, onboardEntity, signalApprove, queryGetState} from './onboard-entity'
 import {OnboardEntityRequest} from '../messages/workflows/v0'
 import crypto from 'crypto'
 import {ApplicationFailure} from '@temporalio/workflow'
+import {ApproveEntityRequest} from '../messages/commands/v0'
 const  assert  = require('assert')
 describe('OnboardEntity', function() {
-
-  describe('given bad arguments', function() {
-    let testEnv: TestWorkflowEnvironment
-    before(async function() {
-      testEnv = await TestWorkflowEnvironment.createLocal({
-        server: {
-          executable: {
-            path: '/opt/homebrew/bin/temporal',
-            type: 'existing-path',
-          }
+  let testEnv: TestWorkflowEnvironment
+  // NOTE we have a single environment for the entire suite
+  // so be careful to use unique workflowIDs for your tests.
+  before(async function() {
+    testEnv = await TestWorkflowEnvironment.createLocal({
+      server: {
+        executable: {
+          // avoid some of the download issues we might encounter
+          path: '/opt/homebrew/bin/temporal',
+          type: 'existing-path',
         }
+      }
+    })
+  })
+  describe('givenValidArgsWithOwnerApprovalNoDeputyOwner', function() {
+    describe('whenApproved', function() {
+      it('shouldBeApproved', function() {
+        let { nativeConnection, client } = testEnv
+        let taskQueue = 'test'
+        let worker = await Worker.create({
+          connection: nativeConnection,
+          taskQueue,
+          workflowsPath: require.resolve('./onboard-entity'),
+        })
+        const args: OnboardEntityRequest = {
+          id: crypto.randomBytes(16).toString('hex'),
+          value: crypto.randomBytes(16).toString('hex'),
+        }
+        worker.run().then(() => {
+          let wfRun = await client.workflow.start(onboardEntity, {
+            taskQueue,
+            workflowId: 'foo',
+            args: [args],
+          })
+          let cmd: ApproveEntityRequest = { comment: 'nocomment'}
+          await wfRun.signal(signalApprove, cmd )
+          // TODO pick up here
+        })
+        await worker.runUntil(async () => {
+          await client.workflow.execute(onboardEntity, {
+            taskQueue,
+            workflowId: 'foo',
+            args: [args],
+          })
+        })
+        await assert.rejects(async() => {
+
+        }, function(e:any): e is string {
+          return e.cause instanceof ApplicationFailure && (e.cause as ApplicationFailure).type == ERR_INVALID_ARGS
+        }, 'failed to receive correct error')
+      })
       })
     })
+  })
+  describe('given bad arguments', function() {
+
     it('should fail correctly', async function (){
       let { nativeConnection, client } = testEnv
       let taskQueue = 'test'
@@ -45,9 +89,8 @@ describe('OnboardEntity', function() {
         return e.cause instanceof ApplicationFailure && (e.cause as ApplicationFailure).type == ERR_INVALID_ARGS
       }, 'failed to receive correct error')
     })
-    after(async function () {
-      testEnv?.teardown()
-    })
   })
-
+  after(async function () {
+    testEnv?.teardown()
+  })
 })
--- onboardings/src/domain/workflows/onboard-entity.ts
+++ onboardings/src/domain/workflows/onboard-entity.ts
@@ -1,29 +1,31 @@
 import {OnboardEntityRequest} from '../messages/workflows/v0'
-import {ApplicationFailure, defineQuery, WorkflowError, workflowInfo} from '@temporalio/workflow';
+import {ApplicationFailure, defineQuery, defineSignal, WorkflowError, workflowInfo} from '@temporalio/workflow';
 import {EntityOnboardingState} from '../messages/queries/v0'
 import * as proto from '@temporalio/proto/protos/root'
+import {ApproveEntityRequest} from '../messages/commands/v0'
 export const queryGetState = defineQuery<EntityOnboardingState>('getState');
+export const signalApprove = defineSignal<[ApproveEntityRequest]>('approve')
 export type OnboardEntity = (params: OnboardEntityRequest) => Promise<void>
 export const ERR_INVALID_ARGS = 'InvalidArgs'
 
 async function assertValidParams(params: OnboardEntityRequest) {
   // poor man's validator
   /*
-       * Temporal is not prescriptive about the strategy you choose for indicating failures in your Workflows.
-       *
-       * We throw an ApplicationFailureException here which would ultimately result in a `WorkflowFailedException`.
-       * This is a common way to fail a Workflow which will never succeed due to bad arguments or some other invariant.
-       *
-       * It is common to use ApplicationFailure for business failures, but these should be considered distinct from an intermittent failure such as
-       * a bug in the code or some dependency which is temporarily unavailable. Temporal can often recover from these kinds of intermittent failures
-       * with a redeployment, downstream service correction, etc. These intermittent failures would typically result in an Exception NOT descended from
-       * TemporalFailure and would therefore NOT fail the Workflow Execution.
-       *
-       * If you have explicit business metrics setup to monitor failed Workflows, you could alternatively return a "Status" result with the business failure
-       * and allow the Workflow Execution to "Complete" without failure.
-       *
-       * Note that `WorkflowFailedException` will count towards the `workflow_failed` SDK Metric (https://docs.temporal.io/references/sdk-metrics#workflow_failed).
-       */
+   * Temporal is not prescriptive about the strategy you choose for indicating failures in your Workflows.
+   *
+   * We throw an ApplicationFailureException here which would ultimately result in a `WorkflowFailedException`.
+   * This is a common way to fail a Workflow which will never succeed due to bad arguments or some other invariant.
+   *
+   * It is common to use ApplicationFailure for business failures, but these should be considered distinct from an intermittent failure such as
+   * a bug in the code or some dependency which is temporarily unavailable. Temporal can often recover from these kinds of intermittent failures
+   * with a redeployment, downstream service correction, etc. These intermittent failures would typically result in an Exception NOT descended from
+   * TemporalFailure and would therefore NOT fail the Workflow Execution.
+   *
+   * If you have explicit business metrics setup to monitor failed Workflows, you could alternatively return a "Status" result with the business failure
+   * and allow the Workflow Execution to "Complete" without failure.
+   *
+   * Note that `WorkflowFailedException` will count towards the `workflow_failed` SDK Metric (https://docs.temporal.io/references/sdk-metrics#workflow_failed).
+   */
   if(!params.id.trim() || !params.value.trim()) {
     throw ApplicationFailure.create({ type: ERR_INVALID_ARGS, message: '`id` and `value` are required properties.'})
   }
