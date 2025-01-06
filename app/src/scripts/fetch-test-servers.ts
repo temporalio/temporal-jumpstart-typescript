793b9a1561323458a334dbe5c6b28b4e1e7436be
Mike Nichols <mike.nichols@temporal.io>
2025-01-06 16:03:54 -0500

    test-server-script

--- app/src/scripts/fetch-test-servers.ts
+++ app/src/scripts/fetch-test-servers.ts
@@ -1,24 +1,46 @@
-const fs = require('fs');
-const download = require('download');
-
-// TODO: WIP: Do not use yet
+import {URL} from 'node:url'
+import os from 'os'
+import download from 'download'
+import path from 'path'
+import fg from 'fast-glob'
+import crypto from 'crypto'
+import * as process from 'node:process'
+import fs from 'fs'
+import {mkdirp} from 'mkdirp'
+// Downloads `temporal-test-server` (the Java SDK implementation) to a tmp dir for the right platform+architecture
+// If `DEST` is provided it'll copy the extracted binary to that location (directory)
 (async () => {
-  const windows = 'platform=windows&sdk-name=sdk-java&sdk-version=latest&arch=amd64'
-  const mac = 'platform=darwin&sdk-name=sdk-java&sdk-version=latest&arch=arm64'
-  await download(`https://temporal.download/temporal-test-server/archive/default?${mac}`,
-    'dist', {
+  const tmpDir = path.join(os.tmpdir(), crypto.randomUUID())
+  const arch = process.env['ARCH'] || os.arch()
+
+  let opts = {
+    'platform': process.env['PLATFORM'] ||  os.platform(), // windows, darwin, linux
+    'sdk-name': 'sdk-java', // sdk-java only
+    'sdk-version': process.env['SDK_VERSION'] || 'latest',
+    'arch': arch // arm64 , amd64
+  }
+  console.log('opts', opts)
+  if(opts.platform.toLowerCase().includes('win32')){
+    opts.platform = 'windows'
+  }
+  if(!opts.arch.toLowerCase().includes('arm64')) {
+    opts.arch = 'amd64' // we only support amd64 or arch64
+  }
+  const url = new URL('https://temporal.download/temporal-test-server/archive/default')
+  url.search = new URLSearchParams(opts).toString()
+  console.log('downloading', url.toString())
+  await download(url.toString(),
+    tmpDir, {
       extract: true,
-    });
-    // this ultimately ends up with a `temporal-test-server` or a `temporal-test-server.exe` inside dist
-  //
-  // fs.writeFileSync('dist/temporal-test-server',
-  //   await download('https://temporal.download/temporal-test-server/archive/default?platform=windows&sdk-name=sdk-java&sdk-version=latest&arch=amd64'), {
-  //   });
-
-  //download('unicorn.com/foo.jpg').pipe(fs.createWriteStream('dist/foo.jpg'));
-
-  // await Promise.all([
-  //   'unicorn.com/foo.jpg',
-  //   'cats.com/dancing.gif'
-  // ].map(url => download(url, 'dist')));
+    })
+  const output= fg.sync(path.join(tmpDir,'*','temporal-test-server*'))
+  const bin = output[0].toString()
+  console.log('downloaded file to:', bin)
+  if(process.env['DEST']) {
+    const destDir = path.resolve(process.env['DEST'])
+    mkdirp.sync(destDir)
+    const dest = path.join(destDir, path.basename(output[0].toString()))
+    console.log('moving binary from "', bin, '" to "', dest ,'"')
+    fs.renameSync(bin, dest)
+  }
 })();
